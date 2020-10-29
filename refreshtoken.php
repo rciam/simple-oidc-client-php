5a391562671e674de8bea3a604f13e1d16ce03d3
nikosev <nikos.ev@hotmail.com>
2020-10-29 18:28:41 +0000

    Extend UI functionalities

--- refreshtoken.php
+++ refreshtoken.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php
     require __DIR__ . '/vendor/autoload.php';
 
     use Jumbojett\OpenIDConnectClient;
+
     require 'config.php';
+    require 'src/MitreIdConnectUtils.php';
+
+    if (!isset($_SESSION)) {
+        session_set_cookie_params(0, '/' . $sessionName);
+        session_name($sessionName);
+        @session_start();
+    }
+
+    if (empty($client_secret)) {
+        $client_secret = null;
+    }
 
     $oidc = new OpenIDConnectClient(
         $issuer,
@@ -15,17 +28,70 @@
     $scopes = array_keys($scopesDefine);
     $oidc->addScope($scopes);
     $oidc->setRedirectURL($redirect_url);
-    $response = array('code');
-    $oidc->setResponseTypes($response);
-    $oidc->authenticate();
-    $refreshToken = $oidc->getRefreshToken();
-    $tokenEndpoint = $oidc->getTokenEndpoint();
-    $curl = "curl -X POST -u '${client_id}':'${client_secret}'  -d 'client_id=${client_id}&client_secret=${client_secret}&grant_type=refresh_token&refresh_token=${refreshToken}&scope=openid%20email%20profile' '${tokenEndpoint}' | python -m json.tool;";
+    $oidc->setResponseTypes(['code']);
+    if (!empty($pkceCodeChallengeMethod)) {
+        $oidc->setCodeChallengeMethod($pkceCodeChallengeMethod);
+    }
+
+    if (isset($_SESSION['sub']) && time() - $_SESSION['CREATED'] < $sessionLifetime) {
+        if (isset($_POST['action'])) {
+            switch ($_POST['action']) {
+                case 'create-refresh-token':
+                    $_SESSION['action'] = 'create-refresh-token';
+                    $scopes[] = "offline_access";
+                    $oidc->addScope($scopes);
+                    $oidc->addAuthParam(['action' => 'create-refresh-token']);
+                    $oidc->authenticate();
+                    break;
+                case 'revoke':
+                    $oidc->revokeToken($_POST['token'], '', $client_id, $client_secret);
+                    $_SESSION['action'] = 'revoke';
+                    if ($_POST['token'] == $_SESSION['refresh_token']) {
+                        $_SESSION['refresh_token'] = null;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (isset($_SESSION['action']) && $_SESSION['action'] == 'create-refresh-token') {
+            $oidc->authenticate();
+            $refreshToken = $oidc->getRefreshToken();
+            $sub = $oidc->requestUserInfo('sub');
+            if ($sub) {
+                $accessToken = $_SESSION['access_token'];
+                $_SESSION['refresh_token'] = $refreshToken;
+            }
+            unset($_SESSION['action']);
+        } else {
+            $accessToken = $_SESSION['access_token'];
+            $refreshToken = $_SESSION['refresh_token'];
+            unset($_SESSION['action']);
+        }
+    } else {
+        $oidc->authenticate();
+        $accessToken = $oidc->getAccessToken();
+        $refreshToken = $oidc->getRefreshToken();
+        $sub = $oidc->requestUserInfo('sub');
+        if ($sub) {
+            $_SESSION['sub'] = $sub;
+            $_SESSION['access_token'] = $accessToken;
+            $_SESSION['refresh_token'] = $refreshToken;
+            $_SESSION['CREATED'] = time();
+        }
+    }
+
+    $openidConfiguration = getMetadata($issuer);
+    $tokenEndpoint = $openidConfiguration->{'token_endpoint'};
+    $userInfoEndpoint = $openidConfiguration->{'userinfo_endpoint'};
+    $introspectionEndpoint = $openidConfiguration->{'introspection_endpoint'};
+
     ?>
     <title><?php echo $title; ?></title>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css" />
 </head>
+
 <body>
     <nav class="navbar sticky-top navbar-expand-xl bg-primary">
         <div class="container d-flex justify-content-between">
@@ -37,34 +103,87 @@
     </nav>
     <br>
     <main role="main" class="container">
-          <div class="jumbotron">
+        <div class="jumbotron">
             <img class="sticky" src="<?php echo $img; ?>" alt="Logo" style="height: 60px; width: 60px; margin-bottom: 20px;">
             <h1 style="display: inline;"><?php echo $title; ?></h1>
             <p style="margin-bottom: 0px;"><b>Client ID: </b> <?php echo $client_id; ?></p>
             <p><b>Client Secret: </b> <?php echo $client_secret; ?></p>
-            <p class="lead" style="margin-bottom: 0px;">Refresh Token: </p>
-            <input id="refreshtoken" size=70 type="text" readonly style="cursor: text;" value="<?php echo $refreshToken; ?>" />
-            <button id="copy" style="cursor: pointer" class="btn btn-copy btn-primary"><i class="icon-file"></i> Copy</button>
-            <p><?php echo $refresh_token_note; ?></p>
-            <p style="margin-bottom: 0px;">To generate access tokens from this refresh token use the following curl command: </p>
-            <input id="curl" size=70 type="text" readonly style="cursor: text;" value="<?php echo $curl; ?>" />
-            <button id="copy2" style="cursor: pointer" class="btn btn-copy btn-primary"><i class="icon-file"></i> Copy</button>
-            <p><?php echo $access_token_note; ?></p>
-            <br>
-            <p><?php echo $manage_token_note; ?><a target="_blank" class="navbar-brand" href="<?php echo $manageTokens; ?>"><?php echo $manageTokens; ?></a></p>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#accessTokenMenu">My Access Token</a></li>
+                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#refreshTokenMenu">My Refresh Tokens</a></li>
+            </ul>
+
+            <div class="tab-content">
+                <div id="accessTokenMenu" class="tab-pane active">
+                    <div>
+                        <p class="lead" style="margin-bottom: 0px;">Access Token: </p>
+                        <input id="accessToken" size=70 type="text" readonly style="cursor: text;" value="<?php echo $accessToken; ?>" />
+                        <button id="copy-mAccessT" style="cursor: pointer" class="btn btn-copy btn-primary"><i class="icon-file"></i> Copy</button>
+                    </div>
+                    <div>
+                        <p style="margin-bottom: 0px;">To get the user info use the following curl command: </p>
+                        <input id="curlUserInfo" size=70 type="text" readonly style="cursor: text;" value="<?php echo getCurlUserInfo($accessToken, $userInfoEndpoint); ?>" />
+                        <button id="copyCurlUserInfo" style="cursor: pointer" class="btn btn-copy btn-primary"><i class="icon-file"></i> Copy</button>
+                    </div>
+                    <div>
+                        <p style="margin-bottom: 0px;">To introspect the token use the following curl command: </p>
+                        <input id="curlIntrospection" size=70 type="text" readonly style="cursor: text;" value="<?php echo getCurlIntrospect($client_id, $client_secret, $accessToken, $introspectionEndpoint); ?>" />
+                        <button id="copyCurlIntrospection" style="cursor: pointer" class="btn btn-copy btn-primary"><i class="icon-file"></i> Copy</button>
+                    </div>
+                    <p><?php echo $access_token_note; ?></p>
+                    <?php if (!empty($refreshToken)) { ?>
+                        <div id="refreshTokenBlock">
+                            <p class="lead" style="margin-bottom: 0px;">Refresh Token: </p>
+                            <input id="refreshToken" size=70 type="text" readonly style="cursor: text;" value="<?php echo $refreshToken; ?>" />
+                            <button id="copyRefreshToken" style="cursor: pointer" class="btn btn-copy btn-primary"><i class="icon-file"></i> Copy</button>
+                            <p><?php echo $refresh_token_note; ?></p>
+                        </div>
+                        <div>
+                            <p style="margin-bottom: 0px;">To generate access tokens from this refresh token use the following curl command: </p>
+                            <input id="curlRefresh" size=70 type="text" readonly style="cursor: text;" value="<?php echo getCurlRefresh($client_id, $client_secret, $scopes, $refreshToken, $tokenEndpoint); ?>" />
+                            <button id="copyCurlRefresh" style="cursor: pointer" class="btn btn-copy btn-primary"><i class="icon-file"></i> Copy</button>
+                            <p><?php echo $access_token_note; ?></p>
+                        </div>
+                        <br>
+                    <?php } else { ?>
+                        <form id="createRefreshToken" action="refreshtoken.php" method="POST">
+                            <input type="hidden" name="action" value="create-refresh-token" />
+                            <input class="btn btn-primary" type="submit" value="Create Refresh Token" />
+                        </form>
+                    <?php } ?>
+                    <p><?php echo $manage_token_note; ?><a target="_blank" class="navbar-brand" href="<?php echo $manageTokens; ?>"><?php echo $manageTokens; ?></a></p>
+                </div>
+                <div id="refreshTokenMenu" class="tab-pane fade">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Value</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php echo getRefreshTokenTable($client_id, $accessToken, $issuer); ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </div>
     </main>
     <script src="vendor/components/jquery/jquery.js"></script>
     <script src="vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
     <script>
-        $('#copy').click(function() {
-            $("#refreshtoken").select();
+        $("#accessTokenMenu").find('.btn-copy').click(function() {
+            $(this).closest('div').find('input').select();
             document.execCommand('copy');
         });
-        $('#copy2').click(function() {
-            $("#curl").select();
+        $("#refreshTokenMenu").find('.btn-copy').click(function() {
+            $(this).closest('tr').find('.token-full').select();
             document.execCommand('copy');
         });
     </script>
 </body>
+
 </html>
---
+++ src/MitreIdConnectUtils.php
@@ -0,0 +1,136 @@
+<?php
+
+function getMetadata($issuer) {
+    $url = $issuer . "/.well-known/openid-configuration/";
+    return json_decode(http($url));;
+}
+
+function getCurlRefresh($clientId, $clientSecret = null, $scopes = null, $refreshToken, $tokenEndpoint) {
+    $command =  "curl -X POST '${tokenEndpoint}' "
+        . (empty($clientSecret) ? "-u '${clientId}':'${clientSecret}' " : "")
+        . "-d 'grant_type=refresh_token"
+        . "&refresh_token=${refreshToken}"
+        . (!empty($clientSecret) ? "&client_id=${clientId}" : "")
+        . ($scopes ? "&scope=" . implode("%20", $scopes) . "' " : "' ")
+        . "| python -m json.tool;";
+
+    return $command;
+}
+
+function getCurlUserInfo($accessToken, $userInfoEndpoint) {
+    $command = "curl ${userInfoEndpoint} "
+        . "-H 'Authorization: Bearer ${accessToken}' "
+        . "-H 'Content-type: application/json' "
+        . "| python -m json.tool;";
+
+    return $command;
+}
+
+function getCurlIntrospect($clientId, $clientSecret, $accessToken, $introspectionEndpoint) {
+    $command = "curl ${introspectionEndpoint} "
+        . (empty($clientSecret) ? "-u '${clientId}':'${clientSecret}' " : "")
+        . "-H 'Content-Type: application/x-www-form-urlencoded' "
+        . "-d 'token=${accessToken}' "
+        . (!empty($clientSecret) ? "-d 'client_id=${clientId}' " : "")
+        . "| python -m json.tool;";
+
+    return $command;
+}
+
+function getActiveRefreshTokens($accessToken, $issuer) {
+    $url = $issuer . "api/tokens/refresh";
+    $headers = [
+        "Authorization: Bearer " . $accessToken,
+    ];
+
+    return json_decode(http($url, null, $headers));
+}
+
+function getRefreshTokenTable($client_id, $accessToken, $issuer) {
+    $activeRefreshTokens = getActiveRefreshTokens($accessToken, $issuer);
+    $index = 1;
+    $table = "";
+
+    foreach ($activeRefreshTokens as $refreshToken) {
+        if ($client_id == $refreshToken->clientId) {
+            $table = $table
+                . "<tr>"
+                . "<th scope=\"row\">${index}</th>"
+                . "<td>"
+                . "    <div class=\"token-value\">"
+                . "        <input style=\"width:100%\" type=\"text\" readonly=\"\" style=\"cursor: text;\" class=\"token-full\" value=\"" . $refreshToken->value . "\">"
+                . "    </div>"
+                . "    <div>"
+                . "        <span>Expire: " . date("Y/m/d", strtotime($refreshToken->expiration)) . "</span>"
+                . "    </div>"
+                . "</td>"
+                . "<td>"
+                . "    <button style=\"cursor: pointer; display: inline-block;\" class=\"btn btn-copy btn-primary\"><i class=\"icon-file\"></i> Copy</button>"
+                . "    <form id=\"revokeRefreshToken\" style=\"display: inline-block;\"action=\"refreshtoken.php\" method=\"POST\">"
+                . "        <input type=\"hidden\" name=\"action\" value=\"revoke\" />"
+                . "        <input type=\"hidden\" name=\"token\" value=\"" . $refreshToken->value . "\" />"
+                . "        <input class=\"btn btn-danger\" type=\"submit\" value=\"Revoke\" />"
+                . "    </form>"
+                . "</td>"
+                . "</tr>";
+            
+            $index++;
+        }
+    }
+
+    return $table;
+}
+
+function http($url, $post_body = null, $headers = [])
+{
+    // create a new cURL resource handle
+    $ch = curl_init();
+
+    // Determine whether this is a GET or POST
+    if ($post_body != null) {
+        // Alows to keep the POST method even after redirect
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+
+        // Default content type is form encoded
+        $content_type = 'application/x-www-form-urlencoded';
+
+        // Determine if this is a JSON payload and add the appropriate content type
+        if (is_object(json_decode($post_body))) {
+            $content_type = 'application/json';
+        }
+
+        // Add POST-specific headers
+        $headers[] = "Content-Type: {$content_type}";
+        $headers[] = 'Content-Length: ' . strlen($post_body);
+    }
+
+    // If we set some heaers include them
+    if (count($headers) > 0) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    }
+
+    // Set URL to download
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // Include header in result? (0 = yes, 1 = no)
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    // Allows to follow redirect
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    // Should cURL return or print out the data? (true = return, false = print)
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Download the given URL, and return output
+    $output = curl_exec($ch);
+
+    // HTTP Response code from server may be required from subclass
+    // $info = curl_getinfo($ch);
+    // $responseCode = $info['http_code'];
+
+    // Close the cURL resource, and free system resources
+    curl_close($ch);
+
+    return $output;
+}
