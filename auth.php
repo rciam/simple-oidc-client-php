470b125e053d4756de27416b83631153089ea6e5
nikosev <nikos.ev@hotmail.com>
2022-01-31 19:19:49 +0200

    Improve `auth.php`

--- auth.php
+++ auth.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php
     require __DIR__ . '/vendor/autoload.php';
 
     use Jumbojett\OpenIDConnectClient;
+
     require 'config.php';
+
+    if (!isset($_SESSION)) {
+        session_set_cookie_params(0, '/' . $sessionName);
+        session_name($sessionName);
+        @session_start();
+    }
+
+    if (empty($client_secret)) {
+        $client_secret = null;
+    }
 
     $oidc = new OpenIDConnectClient(
         $issuer,
@@ -15,23 +27,32 @@
     $scopes = array_keys($scopesDefine);
     $oidc->addScope($scopes);
     $oidc->setRedirectURL($redirect_url);
-    $response = array('code');
-    $oidc->setResponseTypes($response);
+    $oidc->setResponseTypes(['code']);
+    if (!empty($pkceCodeChallengeMethod)) {
+        $oidc->setCodeChallengeMethod($pkceCodeChallengeMethod);
+    }
     $oidc->authenticate();
-    $refreshToken = $oidc->getRefreshToken();
-    $tokenEndpoint = $oidc->getTokenEndpoint();
-	  $userInfo = $oidc->requestUserInfo();
-    $curl = "curl -X POST -u '${client_id}':'${client_secret}'  -d 'client_id=${client_id}&client_secret=${client_secret}&grant_type=refresh_token&refresh_token=${refreshToken}&scope=openid%20email%20profile' '${tokenEndpoint}' | python -m json.tool;";
+    $accessToken = $oidc->getAccessToken();
+    $refreshToken = null;
+    if (in_array('offline_access', $scopes)) {
+        $refreshToken = $oidc->getRefreshToken();
+    }
+
+    $userInfo = $oidc->requestUserInfo();
     ?>
-    <title><?php echo $title; ?></title>
+    <title><?= $title; ?></title>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css" />
 </head>
+
 <body>
     <nav class="navbar sticky-top navbar-expand-xl bg-primary">
         <div class="container d-flex justify-content-between">
-            <a class="navbar-brand" style="color: white" href="index.php"><?php echo $title; ?></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" style="color: white" href="index.php"><?= $title; ?></a>
+            <button
+                class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+            >
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
@@ -39,25 +60,19 @@
     <br>
     <main role="main" class="container">
           <div class="jumbotron">
-            <img class="sticky" src="<?php echo $img; ?>" alt="Logo" style="height: 60px; width: 60px; margin-bottom: 20px;">
-            <h1 style="display: inline;"><?php echo $title; ?></h1>
-            <p style="margin-bottom: 0px;"><b>Client ID: </b> <?php echo $client_id; ?></p>
-            <p><b>User Info: </b> <?php echo var_export($userInfo, true); ?></p>
+            <img class="sticky" src="<?= $img; ?>" alt="Logo" style="height: 60px; width: 60px; margin-bottom: 20px;">
+            <h1 style="display: inline;"><?= $title; ?></h1>
+            <p style="margin-bottom: 0px;"><b>Client ID: </b> <?= $client_id; ?></p>
+            <p><b>User Info: </b> <?= var_export($userInfo, true); ?></p>
+            <p class="lead" style="margin-bottom: 0px;">Access Token: </p>
+            <input id="access_token" size=70 type="text" readonly style="cursor: text;" value="<?= $accessToken; ?>" />
+            <?php if (!empty($refreshToken)) : ?>
             <p class="lead" style="margin-bottom: 0px;">Refresh Token: </p>
-            <input id="refreshtoken" size=70 type="text" readonly style="cursor: text;" value="<?php echo $refreshToken; ?>" />
+            <input id="refresh_token" size=70 type="text" readonly style="cursor: text;" value="<?php echo $refreshToken; ?>" />
+            <?php endif; ?>
         </div>
     </main>
     <script src="vendor/components/jquery/jquery.js"></script>
     <script src="vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
-    <script>
-        $('#copy').click(function() {
-            $("#refreshtoken").select();
-            document.execCommand('copy');
-        });
-        $('#copy2').click(function() {
-            $("#curl").select();
-            document.execCommand('copy');
-        });
-    </script>
 </body>
 </html>
